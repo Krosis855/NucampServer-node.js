39792d62ddc91ad955520d509250d393f9d311d7
Krosis855 <jtspalding22@gmail.com>
2021-09-13 12:06:26 -0700

    Workshop Week 3

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -15,7 +15,10 @@ partnerRouter
       })
       .catch((err) => next(err));
   })
-  .post(authenticate.verifyUser, (req, res, next) => {
+  .post(
+    authenticate.verifyUser, 
+    authenticate.verifyAdmin,
+    (req, res, next) => {
     Partner.create(req.body)
       .then((partner) => {
         console.log("Partner Created ", partner);
@@ -29,7 +32,10 @@ partnerRouter
     res.statusCode = 403;
     res.end("PUT operation not supported on /partners");
   })
-  .delete(authenticate.verifyUser, (req, res, next) => {
+  .delete(
+    authenticate.verifyUser, 
+    authenticate.verifyAdmin,
+    (req, res, next) => {
     Partner.deleteMany()
       .then((response) => {
         res.statusCode = 200;
@@ -54,7 +60,10 @@ partnerRouter
     res.statusCode = 403;
     res.end(`POST operation not supported on /partner/${req.params.partnerId}`);
   })
-  .put(authenticate.verifyUser, (req, res, next) => {
+  .put(
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
     Partner.findByIdAndUpdate(
       req.params.partnerId,
       {
@@ -69,7 +78,10 @@ partnerRouter
       })
       .catch((err) => next(err));
   })
-  .delete(authenticate.verifyUser, (req, res, next) => {
+  .delete(
+    authenticate.verifyUser, 
+    authenticate.verifyAdmin,
+    (req, res, next) => {
     Partner.findByIdAndDelete(req.params.partnerId)
       .then((response) => {
         res.statusCode = 200;
